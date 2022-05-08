54577fe33aa297a739fd2e94621263b8b92a6ed4
Duchies <amanking8938@gmail.com>
2022-05-08 20:12:39 +0530

    cart badge [unfinished]

--- app/src/main/java/com/team4/panstat/StoreFragment.kt
+++ app/src/main/java/com/team4/panstat/StoreFragment.kt
@@ -1,10 +1,12 @@
 package com.team4.panstat
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.team4.panstat.viewmodels.ShopViewModel
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,21 +19,32 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StoreFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private val TAG = "StoreDashBoard"
+    private var cartQuantity = 0
 
+    lateinit var fav: MenuItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        var shopViewModel: ShopViewModel? = null
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
+
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_store, container, false)
@@ -55,5 +68,9 @@ class StoreFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
